7a6b7e586d2f46a09521773e6a7b0c3bd89f913e
KayCheng <kaycheng717@gmail.com>
2017-12-14 20:51:46 +0800

    add photo upload using Carrierwave gem

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -46,7 +46,7 @@ class Admin::RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description, :image)
   end
 
   def set_restaurant
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
+  mount_uploader :image, PhotoUploader
 end
